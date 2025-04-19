c90ccee412ed4a330e65dbbdbb84b660def96577
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-04-19 09:58:37 -0300

    Add files via upload

---
+++ js/revel.js
@@ -0,0 +1,17 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('#Formatura', 
+    {    
+        duration: 1200, reset: false, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('#Esquerda', 
+    {    
+        duration: 1200, reset: false, distance: '100px' , origin: 'left',
+    }
+);
+ScrollReveal().reveal('#Direta', 
+    {    
+        duration: 1500, reset: false, distance: '100px' , origin: 'rigth',
+    }
+);
